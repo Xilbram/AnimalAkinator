5cfe8c6249d427f97764523cdec35ebb72f28075
Xilbram <81371129+Xilbram@users.noreply.github.com>
2022-11-16 20:50:45 -0300

    Arvore pegando em partes
    da p otimizar

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+
+
 class No:
     def __init__(self, data, direita=None, esquerda=None):
         self.data = data
         self.direita = direita
         self.esquerda = esquerda
-        
-        
+
+
 class ArvoreAkinator:
 
     def __init__(self):
         self.raiz = No("macaco")
 
     def inserir_direita(self, no, data):
-        no.direita = No(data)
+        no.direita = data
+
 
     def inserir_esquerda(self, no, data):
         no.esquerda = No(data)
@@ -21,55 +24,71 @@ class Akinator:
     def __init__(self):
         self.arvore = ArvoreAkinator()
 
-    def jogar(self):
 
+    def jogar(self):
         rodando = True
         contador = 0
-
-        print('Pense em um animal')
-        # raiz = self.arvore.raiz  # não estava sendo usado. comentei para ver se há mudança no código
+        print("Pense em um animal")
         no_atual = self.arvore.raiz
+        no_anterior = self.arvore.raiz
         is_animal = True
+        recomecar = True
         msg = no_atual.data
 
         while rodando:
+            if(recomecar == True):
+                no_atual = self.arvore.raiz
+                no_anterior = self.arvore.raiz
+                print("--------------------------------")
+                recomecar = False
 
-            if contador > 0:
-                print('-'*35)
 
-                if isinstance(no_atual, str):
+            if(contador > 0):
+                if(type(no_atual) == str):
                     msg = no_atual
                     is_animal = True
-
                 else:
                     msg = no_atual.data
                     is_animal = False
 
+
+            contador += 1
             usr_resposta = input(f'Seu animal é {msg}? [s/n]: ')
 
-            # se nó for animal, verifica se acertou e prossegue de acordo
-            if is_animal:
+            #se nó for animal, verifica se acertou e prossegue de acordo
+            if(is_animal == True):
                 if usr_resposta.lower() == 's':
                     print('Eu sabia!')
                     rodando = False
 
                 else:
-                    animal_usuario = input('Que animal você pensou? ')
+                    animal_usuario = input("Que animal você pensou? ")
+                    nova_caracteristica = input(f'Como o {animal_usuario} é diferente de {msg}? Diga uma caracteristica: ')
 
-                    # a caracteristica fornecida pelo usuario vira a cabeça, o animal base vai para a esquerda(não)
-                    # e o novo animal para a direita(sim)
+                    #direita é sim, esquerda não(animal antigo vai pra esquerda)
+                    novoNo = No(data = nova_caracteristica, direita = animal_usuario, esquerda = msg)
 
-                    nova_pergunta = input(f'Como o {animal_usuario} é diferente de {msg}? Diga uma caracteristica: ')
+                    if(contador == 1):
+                        self.arvore.raiz.data = nova_caracteristica
+                        self.arvore.raiz.direita = animal_usuario
+                        self.arvore.raiz.esquerda = msg
 
-                    # direita é sim, esquerda não(animal antigo vai pra esquerda)
-                    no_atual = No(data=nova_pergunta, direita=animal_usuario, esquerda=msg)
+                    else:
+                        self.arvore.inserir_direita(no_anterior, novoNo)
 
-            # se nó for pergunta, avança para esquerda/direita
+                    recomecar = True
+
+            #se nó for pergunta, avança para esquerda/direita
             else:
-                # 'sim' indica que deve ir para direita
-                no_atual = no_atual.direita if usr_resposta.lower() == 's' else no_atual.esquerda
+                #sim indica que deve ir para direita
+                if (usr_resposta.lower() == "s"):
+                    no_anterior = no_atual
+                    no_atual = no_atual.direita
 
-            contador += 1
+                else:
+                    no_anterior = no_atual
+                    no_atual = no_atual.esquerda
+
 
 
 jogo = Akinator()
